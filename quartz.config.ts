05ed5402602f64670721ae5591f9e0dfea10b31e
Thanh V. Le <muchuca01@gmail.com>
2023-08-29 02:04:49 +0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "luvata.github.io/aixiv2-new",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
